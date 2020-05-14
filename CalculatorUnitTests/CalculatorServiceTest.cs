c7c808dac41bb752fdd793bc6a49cd99b1112707
Nicolás Colman <nicolasecolman@gmail.com>
2020-05-14 08:23:32 -0300

    Unit tests.

---
+++ CalculatorUnitTests/CalculatorServiceTest.cs
@@ -0,0 +1,79 @@
+using BlazorCalculator.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace CalculatorUnitTests
+{
+    [TestClass]
+    public class CalculatorServiceTest
+    {
+        ICalculatorService sut = null;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            //We use the implementation for testing because we don't have a database here
+            sut = new CalculatorService();
+        }
+
+        [TestMethod]
+        public void Add()
+        {
+            var number1 = 1;
+            var number2 = 1;
+            var expected = 2;
+            var actual = sut.Add(number1, number2);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
+        public void Subtract()
+        {
+            var number1 = 1;
+            var number2 = 1;
+            var expected = 0;
+            var actual = sut.Subtract(number1, number2);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
+        public void Multiply()
+        {
+            var number1 = 2;
+            var number2 = 3;
+            var expected = 6;
+            var actual = sut.Multiply(number1, number2);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
+        public void Divide()
+        {
+            var number1 = 10;
+            var number2 = 5;
+            var expected = 2;
+            var actual = sut.Divide(number1, number2);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void DivideByZero()
+        {
+            var number1 = 1;
+            var number2 = 0;
+            var actual = sut.Divide(number1, number2);
+
+        }
+
+
+    }
+}
